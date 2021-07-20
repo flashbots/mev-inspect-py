0e45f22de8b9e44b16f08ab832e0da39d711b151
Luke Van Seters <lukevanseters@gmail.com>
2021-07-20 19:40:44 -0400

    Add output to testing file to make sure all looks good

--- testing_file.py
+++ testing_file.py
@@ -24,6 +24,12 @@ base_provider = Web3.HTTPProvider(args.rpc)
 
 ## Get block data that we need
 block_data = block.create_from_block_number(args.block_number[0], base_provider)
+print(f"Total traces: {len(block_data.traces)}")
+
+total_transactions = len(
+    set(t.transaction_hash for t in block_data.traces if t.transaction_hash is not None)
+)
+print(f"Total transactions: {total_transactions}")
 
 ## Build a Uniswap inspector
 uniswap_inspector = UniswapInspector(base_provider)
@@ -31,4 +37,5 @@ uniswap_inspector = UniswapInspector(base_provider)
 ## Create a processor, pass in an ARRAY of inspects
 processor = Processor([uniswap_inspector, uniswap_inspector])
 
-processor.get_transaction_evaluations(block_data)
+classifications = processor.get_transaction_evaluations(block_data)
+print(f"Returned {len(classifications)} classifications")
