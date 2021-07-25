dc583d4d0792cfca5c834353a361ae60751c0bc0
Luke Van Seters <lukevanseters@gmail.com>
2021-07-25 19:46:57 -0400

    Gut processor. Make a simple classification object

--- mev_inspect/processor.py
+++ mev_inspect/processor.py
@@ -1,43 +1,28 @@
 from typing import List
 
-from mev_inspect.inspectors import Inspector
-from mev_inspect.schemas.blocks import Block, NestedTrace, TraceType
+from mev_inspect.schemas.blocks import Block, TraceType
 from mev_inspect.schemas.classifications import (
     Classification,
-    UnknownClassification,
+    ClassificationType,
 )
-from mev_inspect.traces import as_nested_traces
 
 
 class Processor:
-    def __init__(self, inspectors: List[Inspector]) -> None:
-        self._inspectors = inspectors
+    def __init__(self) -> None:
+        pass
 
-    def get_transaction_evaluations(
+    def process(
         self,
         block: Block,
     ) -> List[Classification]:
-        transaction_traces = (
-            trace for trace in block.traces if trace.type != TraceType.reward
-        )
-
         return [
-            self._run_inspectors(nested_trace)
-            for nested_trace in as_nested_traces(transaction_traces)
+            Classification(
+                transaction_hash=trace.transaction_hash,
+                block_number=trace.block_number,
+                trace_type=trace.type,
+                trace_address=trace.trace_address,
+                classification_type=ClassificationType.unknown,
+            )
+            for trace in block.traces
+            if trace.type != TraceType.reward
         ]
-
-    def _run_inspectors(self, nested_trace: NestedTrace) -> Classification:
-        for inspector in self._inspectors:
-            classification = inspector.inspect(nested_trace)
-
-            if classification is not None:
-                return classification
-
-        internal_classifications = [
-            self._run_inspectors(subtrace) for subtrace in nested_trace.subtraces
-        ]
-
-        return UnknownClassification(
-            trace=nested_trace.trace,
-            internal_classifications=internal_classifications,
-        )
--- mev_inspect/schemas/classifications.py
+++ mev_inspect/schemas/classifications.py
@@ -1,14 +1,18 @@
+from enum import Enum
 from typing import List
 
 from pydantic import BaseModel
 
-from .blocks import Trace
+from .blocks import TraceType
+
+
+class ClassificationType(Enum):
+    unknown = "unknown"
 
 
 class Classification(BaseModel):
-    pass
-
-
-class UnknownClassification(Classification):
-    trace: Trace
-    internal_classifications: List[Classification]
+    transaction_hash: str
+    block_number: int
+    trace_type: TraceType
+    trace_address: List[int]
+    classification_type: ClassificationType
