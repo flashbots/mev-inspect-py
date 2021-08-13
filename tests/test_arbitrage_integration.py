e4f47c0e42e6151a39d8068357ae672d4e5b31d5
Patrick Daly <pmdaly12@gmail.com>
2021-08-13 14:14:14 -0700

    correcting an import path

--- tests/test_arbitrage_integration.py
+++ tests/test_arbitrage_integration.py
@@ -1,5 +1,5 @@
-from mev_inspect.classifier_specs import CLASSIFIER_SPECS
-from mev_inspect.trace_classifier import TraceClassifier
+from mev_inspect.classifiers.specs import ALL_CLASSIFIER_SPECS
+from mev_inspect.classifiers.trace import TraceClassifier
 from mev_inspect.arbitrage import get_arbitrages
 from mev_inspect.swaps import get_swaps
 
@@ -9,7 +9,7 @@ from .utils import load_test_block
 def test_arbitrage_real_block():
     block = load_test_block(12914994)
 
-    trace_clasifier = TraceClassifier(CLASSIFIER_SPECS)
+    trace_clasifier = TraceClassifier(ALL_CLASSIFIER_SPECS)
     classified_traces = trace_clasifier.classify(block.traces)
 
     swaps = get_swaps(classified_traces)
