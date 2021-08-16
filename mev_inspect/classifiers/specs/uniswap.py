70d0502ac48f1412a48ec8a7ff5b1fc6f2dc0bab
Luke Van Seters <lukevanseters@gmail.com>
2021-08-16 15:25:16 -0400

    Add them to the final list

--- mev_inspect/classifiers/specs/uniswap.py
+++ mev_inspect/classifiers/specs/uniswap.py
@@ -9,7 +9,7 @@ ZEROX_CONTRACT_SPECS = [
     ClassifierSpec(
         abi_name="exchangeProxy",
         protocol=Protocol.zerox,
-        valid_contract_addresses=["0xdef1c0ded9bec7f1a1670819833240f027b25eff"],
+        valid_contract_addresses=["0xDef1C0ded9bec7F1a1670819833240f027b25EfF"],
     ),
     ClassifierSpec(
         abi_name="exchangeProxyAllowanceTarget",
