cc9f3e993dbd6a58b63f910975142155608f50f0
Gui Heise <gheise@ufl.edu>
2021-09-16 11:20:28 -0400

    Fixed helpers default attrs

--- mev_inspect/crud/classified_traces.py
+++ mev_inspect/crud/classified_traces.py
@@ -26,24 +26,22 @@ def write_classified_traces(
     for trace in classified_traces:
         models.append(
             ClassifiedTraceModel(
-                **{
-                    "transaction_hash": trace.transaction_hash,
-                    "block_number": trace.block_number,
-                    "classification": trace.classification.value,
-                    "trace_type": trace.type.value,
-                    "trace_address": trace.trace_address,
-                    "protocol": str(trace.protocol),
-                    "abi_name": trace.abi_name,
-                    "function_name": trace.function_name,
-                    "function_signature": trace.function_signature,
-                    "inputs": json.loads(trace.json(include={"inputs"}))["inputs"],
-                    "from_address": trace.from_address,
-                    "to_address": trace.to_address,
-                    "gas": trace.gas,
-                    "value": trace.value,
-                    "gas_used": trace.gas_used,
-                    "error": trace.error,
-                }
+                transaction_hash=trace.transaction_hash,
+                block_numbe=trace.block_number,
+                classification=trace.classification.value,
+                trace_type=trace.type.value,
+                trace_address=trace.trace_address,
+                protocol=str(trace.protocol),
+                abi_name=trace.abi_name,
+                function_name=trace.function_name,
+                function_signature=trace.function_signature,
+                inputs=json.loads(trace.json(include={"inputs"}))["inputs"],
+                from_address=trace.from_address,
+                to_address=trace.to_address,
+                gas=trace.gas,
+                value=trace.value,
+                gas_used=trace.gas_used,
+                error=trace.error,
             )
         )
 
