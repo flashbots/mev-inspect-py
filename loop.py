5682c2ce4ec64c0be2827e37b9e3947fe23b17c5
Luke Van Seters <lukevanseters@gmail.com>
2021-09-20 12:31:27 -0400

    Break out block listener to a start / stop daemon instead of running on startup

---
+++ loop.py
@@ -0,0 +1,22 @@
+import logging
+import time
+
+from mev_inspect.signal_handler import GracefulKiller
+
+
+logging.basicConfig(filename="loop.log", level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+def run():
+    logger.info("Starting...")
+
+    killer = GracefulKiller()
+    while not killer.kill_now:
+        time.sleep(1)
+
+    logger.info("Stopping...")
+
+
+if __name__ == "__main__":
+    run()
