173d16c2bc4007cc4a56f21c9124214ce582533e
Gui Heise <gheise@ufl.edu>
2021-09-20 16:17:03 -0400

    Add docstrings to new functions

--- mev_inspect/aave_liquidations.py
+++ mev_inspect/aave_liquidations.py
@@ -20,6 +20,7 @@ def find_liquidations_from_traces(
     traces: List[ClassifiedTrace],
 ) -> List[Liquidation]:
     """Inspect list of classified traces and identify liquidation"""
+
     seen_transactions: List[str] = []
     liquidations: List[ClassifiedTrace] = []
     result: List[Liquidation] = []
