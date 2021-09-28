f87016a561458bc269686107292534ebfda5da76
Luke Van Seters <lukevanseters@gmail.com>
2021-09-28 12:55:03 -0400

    Add field for transfer classifiers

--- mev_inspect/schemas/classifier_specs.py
+++ mev_inspect/schemas/classifier_specs.py
@@ -1,8 +1,15 @@
-from typing import Dict, List, Optional
+from typing import Callable, Dict, List, Optional
 
 from pydantic import BaseModel
 
-from .classified_traces import Classification, Protocol
+from .classified_traces import Classification, DecodedCallTrace, Protocol
+from .transfers import Transfer
+
+
+TransferClassifier = Callable[
+    [DecodedCallTrace, List[DecodedCallTrace], List[DecodedCallTrace]],
+    Transfer,
+]
 
 
 class ClassifierSpec(BaseModel):
@@ -10,3 +17,4 @@ class ClassifierSpec(BaseModel):
     protocol: Optional[Protocol] = None
     valid_contract_addresses: Optional[List[str]] = None
     classifications: Dict[str, Classification] = {}
+    transfer_classifiers: Dict[str, TransferClassifier] = {}
