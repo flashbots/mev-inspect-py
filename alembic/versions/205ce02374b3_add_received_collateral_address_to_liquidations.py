f84d192053f90cc38610f6365c1e89f53823f043
Gui Heise <gheise@mac.local>
2021-10-04 18:59:58 -0400

    Adjust migration to add column

--- alembic/versions/205ce02374b3_add_received_collateral_address_to_liquidations.py
+++ alembic/versions/205ce02374b3_add_received_collateral_address_to_liquidations.py
@@ -17,21 +17,9 @@ depends_on = None
 
 
 def upgrade():
-    op.create_table(
+    op.add_column(
         "liquidations",
-        sa.Column("created_at", sa.TIMESTAMP, server_default=sa.func.now()),
-        sa.Column("liquidated_user", sa.String(256), nullable=False),
-        sa.Column("liquidator_user", sa.String(256), nullable=False),
-        sa.Column("collateral_token_address", sa.String(256), nullable=False),
-        sa.Column("debt_token_address", sa.String(256), nullable=False),
-        sa.Column("debt_purchase_amount", sa.Numeric, nullable=False),
-        sa.Column("received_amount", sa.Numeric, nullable=False),
-        sa.Column("received_collateral_address", sa.String(256), nullable=True),
-        sa.Column("protocol", sa.String(256), nullable=True),
-        sa.Column("transaction_hash", sa.String(66), nullable=False),
-        sa.Column("trace_address", sa.String(256), nullable=False),
-        sa.Column("block_number", sa.Numeric, nullable=False),
-        sa.PrimaryKeyConstraint("transaction_hash", "trace_address"),
+        sa.Column("received_token_address", sa.String(256), nullable=True),
     )
 
 
