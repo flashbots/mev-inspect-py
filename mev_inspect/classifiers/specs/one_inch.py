3ddfcfdab030bd270b40264223ae774ba9b8ebe7
Leo Arias <elopio@openzeppelin.com>
2021-10-07 18:35:01 +0000

    Classifiers: add 1inch Limit Order Protocol

---
+++ mev_inspect/classifiers/specs/one_inch.py
@@ -0,0 +1,37 @@
+from mev_inspect.schemas.classified_traces import (
+    DecodedCallTrace,
+    Protocol,
+)
+from mev_inspect.schemas.classifiers import (
+    ClassifierSpec,
+    SwapClassifier,
+)
+
+
+class OneInchFillOrderProtocolSwapClassifier(SwapClassifier):
+
+    @staticmethod
+    def get_swap_recipient(trace: DecodedCallTrace) -> str:
+        return trace.from_address
+
+
+FILL_ORDER_SIGNATURE = (
+    "fillOrder((uint256,address,address,bytes,bytes,bytes,bytes,bytes,bytes,bytes),"
+    "bytes,uint256,uint256,uint256)"
+)
+
+
+FILL_ORDER_SIGNATURE = (
+    "fillOrder((uint256,address,address,bytes,bytes,bytes,bytes,bytes,bytes,bytes),"
+    "bytes,uint256,uint256,uint256)"
+)
+
+ONE_INCH_LIMIT_ORDER_PROTOCOL_SPEC = ClassifierSpec(
+    abi_name="1inchLimitOrderProtocol",
+    protocol=Protocol.one_inch,
+    classifications={
+        FILL_ORDER_SIGNATURE: OneInchFillOrderProtocolSwapClassifier,
+    },
+)
+
+ONE_INCH_CLASSIFIER_SPECS = [ONE_INCH_LIMIT_ORDER_PROTOCOL_SPEC]
