a75bc69366c6bb9146feb11209bd36aa68b6d589
Gui Heise <gheise@ufl.edu>
2021-10-12 14:19:07 -0400

    aTokens classifer spec

--- mev_inspect/classifiers/specs/aave.py
+++ mev_inspect/classifiers/specs/aave.py
@@ -15,4 +15,13 @@ AAVE_SPEC = ClassifierSpec(
     },
 )
 
+ATOKENS_SPEC = ClassifierSpec(
+    abi_name="aTokens",
+    protocol=Protocol.aave,
+    classifications={
+        "transferOnLiquidation(address,address,uint256)": Classification.transfer,
+        "transferFrom(address,address,uint256)": Classification.transfer,
+    },
+)
+
 AAVE_CLASSIFIER_SPECS = [AAVE_SPEC]
