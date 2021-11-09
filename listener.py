63e81b22e6d5a34bdbf726e13ce3aa792ac6c81b
Luke Van Seters <lukevanseters@gmail.com>
2021-11-09 18:21:51 -0500

    Ping healthcheck url on each successful block inspect

--- listener.py
+++ listener.py
@@ -2,6 +2,8 @@ import asyncio
 import logging
 import os
 
+import aiohttp
+
 from mev_inspect.block import get_latest_block_number
 from mev_inspect.concurrency import coro
 from mev_inspect.crud.latest_block_update import (
@@ -27,6 +29,8 @@ async def run():
     if rpc is None:
         raise RuntimeError("Missing environment variable RPC_URL")
 
+    healthcheck_url = os.getenv("LISTENER_HEALTHCHECK_URL")
+
     logger.info("Starting...")
 
     killer = GracefulKiller()
@@ -35,36 +39,57 @@ async def run():
     trace_db_session = get_trace_session()
 
     inspector = MEVInspector(rpc, inspect_db_session, trace_db_session)
-
     base_provider = get_base_provider(rpc)
 
-    latest_block_number = await get_latest_block_number(base_provider)
-
     while not killer.kill_now:
-        last_written_block = find_latest_block_update(inspect_db_session)
-        logger.info(f"Latest block: {latest_block_number}")
-        logger.info(f"Last written block: {last_written_block}")
-
-        if last_written_block is None:
-            # maintain lag if no blocks written yet
-            last_written_block = latest_block_number - 1
-
-        if last_written_block < (latest_block_number - BLOCK_NUMBER_LAG):
-            block_number = (
-                latest_block_number
-                if last_written_block is None
-                else last_written_block + 1
-            )
-
-            logger.info(f"Writing block: {block_number}")
-
-            await inspector.inspect_single_block(block=block_number)
-            update_latest_block(inspect_db_session, block_number)
-        else:
-            await asyncio.sleep(5)
-            latest_block_number = await get_latest_block_number(base_provider)
+        await inspect_next_block(
+            inspector,
+            inspect_db_session,
+            base_provider,
+            healthcheck_url,
+        )
 
     logger.info("Stopping...")
+
+
+async def inspect_next_block(
+    inspector: MEVInspector,
+    inspect_db_session,
+    base_provider,
+    healthcheck_url,
+):
+    latest_block_number = await get_latest_block_number(base_provider)
+    last_written_block = find_latest_block_update(inspect_db_session)
+
+    logger.info(f"Latest block: {latest_block_number}")
+    logger.info(f"Last written block: {last_written_block}")
+
+    if last_written_block is None:
+        # maintain lag if no blocks written yet
+        last_written_block = latest_block_number - 1
+
+    if last_written_block < (latest_block_number - BLOCK_NUMBER_LAG):
+        block_number = (
+            latest_block_number
+            if last_written_block is None
+            else last_written_block + 1
+        )
+
+        logger.info(f"Writing block: {block_number}")
+
+        await inspector.inspect_single_block(block=block_number)
+        update_latest_block(inspect_db_session, block_number)
+
+        if healthcheck_url:
+            await ping_healthcheck_url(healthcheck_url)
+    else:
+        await asyncio.sleep(5)
+
+
+async def ping_healthcheck_url(url):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url):
+            pass
 
 
 if __name__ == "__main__":
