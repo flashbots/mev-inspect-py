0c69828a6fc7632ea3a20cfaaf1e67e781d37149
Robert Miller <bert.c.miller@gmail.com>
2021-11-10 20:03:55 -0500

    add open sea trace classifier

---
+++ mev_inspect/classifiers/specs/opensea.py
@@ -0,0 +1,15 @@
+from mev_inspect.schemas.traces import (
+    Protocol,
+)
+from mev_inspect.schemas.classifiers import (
+    ClassifierSpec,
+)
+
+
+OPEN_SEA_SPEC = [
+    ClassifierSpec(
+        abi_name="OpenSea",
+        protocol=Protocol.opensea,
+        valid_contract_addresses=["0x7be8076f4ea4a4ad08075c2508e481d6c946d12b"],
+    )
+]
