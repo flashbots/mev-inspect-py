5d03c1fbfa8b478aff85ca18c89b7611d90b58e3
Gui Heise <gheise@ufl.edu>
2021-11-11 10:39:24 -0500

    Add classifier specs to init

--- mev_inspect/classifiers/specs/cryptopunks.py
+++ mev_inspect/classifiers/specs/cryptopunks.py
@@ -7,7 +7,9 @@ from mev_inspect.schemas.classifiers import (
 )
 
 CRYPTO_PUNKS_SPEC = ClassifierSpec(
-    abi_name='cryptopunks',
+    abi_name="cryptopunks",
     protocol=Protocol.cryptopunks,
-    valid_contract_addresses=["0xb47e3cd837dDF8e4c57F05d70Ab865de6e193BBB"]
+    valid_contract_addresses=["0xb47e3cd837dDF8e4c57F05d70Ab865de6e193BBB"],
 )
+
+CRYPTOPUNKS_CLASSIFIER_SPECS = [CRYPTO_PUNKS_SPEC]
