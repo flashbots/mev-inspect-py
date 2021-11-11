af2aab4940387d221413af584eff136bdff3f19c
Robert Miller <bert.c.miller@gmail.com>
2021-11-10 20:14:42 -0500

    add cryptopunks trace classifier

---
+++ mev_inspect/classifiers/specs/cryptopunks.py
@@ -0,0 +1,13 @@
+from mev_inspect.schemas.traces import (
+    Protocol,
+)
+
+from mev_inspect.schemas.classifiers import (
+    ClassifierSpec,
+)
+
+CRYPTO_PUNKS_SPEC = ClassifierSpec(
+    abi_name='cryptopunks',
+    protocol=Protocol.cryptopunks,
+    valid_contract_addresses=["0xb47e3cd837dDF8e4c57F05d70Ab865de6e193BBB"]
+)
--- mev_inspect/schemas/traces.py
+++ mev_inspect/schemas/traces.py
@@ -44,6 +44,7 @@ class Protocol(Enum):
     balancer_v1 = "balancer_v1"
     compound_v2 = "compound_v2"
     cream = "cream"
+    cryptopunks = "cryptopunks"
 
 
 class ClassifiedTrace(Trace):
