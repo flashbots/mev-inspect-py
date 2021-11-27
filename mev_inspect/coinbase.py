4f34316afb7554082822dbe54dda7ff98d8610f1
Luke Van Seters <lukevanseters@gmail.com>
2021-11-26 21:03:57 -0500

    COINBASE_TOKEN_NAMES => COINBASE_TOKEN_NAME_BY_ADDRESS

--- mev_inspect/coinbase.py
+++ mev_inspect/coinbase.py
@@ -6,17 +6,17 @@ from mev_inspect.schemas.coinbase import CoinbasePrices, CoinbasePricesResponse
 
 
 COINBASE_API_BASE = "https://www.coinbase.com/api/v2"
-COINBASE_TOKEN_NAMES = {
+COINBASE_TOKEN_NAME_BY_ADDRESS = {
     WETH_ADDRESS: "weth",
     ETH_TOKEN_ADDRESS: "ethereum",
 }
 
 
 async def fetch_coinbase_prices(token_address: str) -> CoinbasePrices:
-    if token_address not in COINBASE_TOKEN_NAMES:
+    if token_address not in COINBASE_TOKEN_NAME_BY_ADDRESS:
         raise ValueError(f"Unsupported token_address {token_address}")
 
-    coinbase_token_name = COINBASE_TOKEN_NAMES[token_address]
+    coinbase_token_name = COINBASE_TOKEN_NAME_BY_ADDRESS[token_address]
     url = f"{COINBASE_API_BASE}/assets/prices/{coinbase_token_name}"
 
     async with aiohttp.ClientSession() as session:
