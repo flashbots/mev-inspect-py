94a05d8845f6668d29ede83ca971721cbc5d56fe
Luke Van Seters <lukevanseters@gmail.com>
2021-12-14 13:09:28 -0500

    Run isort for alembic

--- alembic/env.py
+++ alembic/env.py
@@ -1,8 +1,8 @@
 from logging.config import fileConfig
 
+from alembic import context
 from sqlalchemy import engine_from_config, pool
 
-from alembic import context
 from mev_inspect.db import get_inspect_database_uri
 
 
