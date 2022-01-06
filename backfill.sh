b33eb49dd2e6848a8bf5b95bfbfdb085e1d33b92
Luke Van Seters <lukevanseters@gmail.com>
2022-01-06 17:10:52 -0500

    Remove old backfill code

--- backfill.sh
+++
@@ -1,6 +0,0 @@
-current_image=$(kubectl get deployment mev-inspect -o=jsonpath='{$.spec.template.spec.containers[:1].image}')
-
-helm template mev-inspect-backfill ./k8s/mev-inspect-backfill \
-    --set image.repository=$current_image \
-    --set command.startBlockNumber=$1 \
-    --set command.endBlockNumber=$2 | kubectl apply -f -
