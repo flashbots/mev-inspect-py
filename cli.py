b1d4cb852b9db93113c96494df8803bc4fbaff63
Luke Van Seters <lukevanseters@gmail.com>
2022-02-04 15:30:20 -0500

    Add some logging. Remove unused list function

--- cli.py
+++ cli.py
@@ -13,7 +13,7 @@ from mev_inspect.inspector import MEVInspector
 from mev_inspect.prices import fetch_prices, fetch_prices_range
 from mev_inspect.queue.broker import connect_broker
 from mev_inspect.queue.tasks import inspect_many_blocks_task
-from mev_inspect.s3_export import export_block_range, list_export_bucket
+from mev_inspect.s3_export import export_block_range
 
 RPC_URL_ENV = "RPC_URL"
 
@@ -126,8 +126,6 @@ def fetch_all_prices():
 @click.argument("before_block_number", type=int)
 def s3_export(after_block_number: int, before_block_number: int):
     inspect_db_session = get_inspect_session()
-
-    print(list_export_bucket())
     export_block_range(inspect_db_session, after_block_number, before_block_number)
 
 
