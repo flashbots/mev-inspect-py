ee93eba7e48f172324be58ebd75489898d6da108
Luke Van Seters <lukevanseters@gmail.com>
2022-02-04 15:20:59 -0500

    Run query. Export to S3

--- cli.py
+++ cli.py
@@ -10,7 +10,7 @@ from mev_inspect.crud.prices import write_prices
 from mev_inspect.db import get_inspect_session, get_trace_session
 from mev_inspect.inspector import MEVInspector
 from mev_inspect.prices import fetch_prices, fetch_prices_range
-from mev_inspect.s3_export import list_export_bucket
+from mev_inspect.s3_export import export_block_range, list_export_bucket
 
 RPC_URL_ENV = "RPC_URL"
 
@@ -123,10 +123,10 @@ def fetch_all_prices():
 @click.argument("after_block_number", type=int)
 @click.argument("before_block_number", type=int)
 def s3_export(after_block_number: int, before_block_number: int):
-    logger.info(after_block_number)
-    logger.info(before_block_number)
+    inspect_db_session = get_inspect_session()
 
     print(list_export_bucket())
+    export_block_range(inspect_db_session, after_block_number, before_block_number)
 
 
 @cli.command()
