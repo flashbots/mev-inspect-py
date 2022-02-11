95444eae24c0e8582a00902b6170a0aca80b1dab
Gui Heise <gheise@ufl.edu>
2022-02-11 18:43:39 -0500

    Add actor

--- cli.py
+++ cli.py
@@ -13,7 +13,6 @@ from mev_inspect.inspector import MEVInspector
 from mev_inspect.prices import fetch_prices, fetch_prices_range
 from mev_inspect.queue.broker import connect_broker
 from mev_inspect.queue.tasks import inspect_many_blocks_task
-from mev_inspect.s3_export import export_block
 
 RPC_URL_ENV = "RPC_URL"
 
@@ -131,14 +130,6 @@ def fetch_all_prices():
 
     logger.info("Writing prices")
     write_prices(inspect_db_session, prices)
-
-
-@cli.command()
-@click.argument("after_block_number", type=int)
-@click.argument("before_block_number", type=int)
-def s3_export(block_number: int):
-    inspect_db_session = get_inspect_session()
-    export_block(inspect_db_session, block_number)
 
 
 @cli.command()
