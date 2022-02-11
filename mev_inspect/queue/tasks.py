bb06c8a958d4fe90d368bbf868e937db4cc4b9f1
Gui Heise <gheise@ufl.edu>
2022-02-11 16:47:24 -0500

    Add export task

--- mev_inspect/queue/tasks.py
+++ mev_inspect/queue/tasks.py
@@ -2,6 +2,8 @@ import asyncio
 import logging
 from contextlib import contextmanager
 
+from mev_inspect.s3_export import export_block
+
 from .middleware import DbMiddleware, InspectorMiddleware
 
 logger = logging.getLogger(__name__)
@@ -21,6 +23,11 @@ def inspect_many_blocks_task(
                     before_block=before_block,
                 )
             )
+
+
+def export_block_task(block_number: int):
+    with _session_scope(DbMiddleware.get_inspect_sessionmaker()) as inspect_db_session:
+        export_block(inspect_db_session, block_number)
 
 
 @contextmanager
