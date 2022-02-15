94f4ec7d40043e58e66eecbe6f938d8e846543a1
Luke Van Seters <lukevanseters@gmail.com>
2022-02-15 10:28:06 -0500

    Fix priorities. Lower comes first

--- mev_inspect/queue/tasks.py
+++ mev_inspect/queue/tasks.py
@@ -9,10 +9,11 @@ from .middleware import DbMiddleware, InspectorMiddleware
 logger = logging.getLogger(__name__)
 
 
+# when we have export backfill,
 # create a separate actor for backfill export
-# set to same priority as backfill inspect
-LIVE_EXPORT_BLOCK_PRIORITY = 1
-BACKFILL_INSPECT_MANY_BLOCKS_PRIORITY = 0
+# and set to same priority as backfill inspect
+LIVE_EXPORT_BLOCK_PRIORITY = 0
+BACKFILL_INSPECT_MANY_BLOCKS_PRIORITY = 1
 
 
 def inspect_many_blocks_task(
