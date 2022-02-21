c6e6d694ec37e21388e7fb7a41216941acf4e891
Gui Heise <gheise@ufl.edu>
2022-02-21 11:02:30 -0500

    Add task to the listener

--- listener.py
+++ listener.py
@@ -18,7 +18,7 @@ from mev_inspect.queue.broker import connect_broker
 from mev_inspect.queue.tasks import (
     HIGH_PRIORITY,
     HIGH_PRIORITY_QUEUE,
-    export_block_task,
+    realtime_export_task,
 )
 from mev_inspect.signal_handler import GracefulKiller
 
