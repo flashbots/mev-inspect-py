cbad9e79b625df9428ecb9d1f7b9a7673d874a2d
Gui Heise <gheise@ufl.edu>
2022-02-21 10:55:26 -0500

    Separate tasks

--- mev_inspect/queue/tasks.py
+++ mev_inspect/queue/tasks.py
@@ -37,6 +37,16 @@ def export_block_task(block_number: int):
         export_block(inspect_db_session, block_number)
 
 
+def realtime_export_task(block_number: int):
+    with _session_scope(DbMiddleware.get_inspect_sessionmaker()) as inspect_db_session:
+        export_block(inspect_db_session, block_number)
+
+
+def backfill_export_task(block_number: int):
+    with _session_scope(DbMiddleware.get_inspect_sessionmaker()) as inspect_db_session:
+        export_block(inspect_db_session, block_number)
+
+
 @contextmanager
 def _session_scope(Session=None):
     if Session is None:
