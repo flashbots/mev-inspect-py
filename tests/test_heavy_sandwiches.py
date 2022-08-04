458ad7f7f5a1169f9f6ec45271444d742b1e8d71
Gui Heise <gheise@ufl.edu>
2022-08-04 11:00:09 -0400

    Add test for front heavy sandwiches

--- tests/test_heavy_sandwiches.py
+++ tests/test_heavy_sandwiches.py
@@ -23,3 +23,20 @@ def test_back_heavy_sandwich_profits(trace_classifier: TraceClassifier):
         if observed_sandwich.sandwicher_address == expected_sandwicher:
             assert expected_token_address == observed_sandwich.profit_token_address
             assert expected_profit_amount == observed_sandwich.profit_amount
+
+
+def test_front_heavy_sandwich_profits(trace_classifier: TraceClassifier):
+    block_number = 14659109
+    expected_sandwicher = "0x01ff6318440f7d5553a82294d78262d5f5084eff"
+    expected_token_address = "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2"
+    expected_profit_amount = -180511102573164864
+
+    block = load_test_block(block_number)
+    classified_traces = trace_classifier.classify(block.traces)
+    swaps: List[Swap] = get_swaps(classified_traces)
+    result: List[Sandwich] = get_sandwiches(swaps)
+
+    for observed_sandwich in result:
+        if observed_sandwich.sandwicher_address == expected_sandwicher:
+            assert expected_token_address == observed_sandwich.profit_token_address
+            assert expected_profit_amount == observed_sandwich.profit_amount
