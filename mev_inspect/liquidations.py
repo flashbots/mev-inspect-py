d5ffa4dc46dc2b8695a49c9fd850d2e3af415d98
pintail-xyz <192824+pintail-xyz@users.noreply.github.com>
2022-10-10 00:06:44 +0100

    Revert "ignore reverted transactions from liquidation classifier"
    This reverts commit 397c7536fc10934f18a02e8c81ccab296985d755.

--- mev_inspect/liquidations.py
+++ mev_inspect/liquidations.py
@@ -30,9 +30,6 @@ def get_liquidations(classified_traces: List[ClassifiedTrace]) -> List[Liquidati
         if _is_child_liquidation(trace, parent_liquidations):
             continue
 
-        if trace.error == "Reverted":
-            continue
-
         if trace.classification == Classification.liquidate:
 
             parent_liquidations.append(trace)
